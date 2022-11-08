230ffbfdb42e7eed91ddbeecdbc0dc7706faad46
Divyansh <divyanshgupta1106@gmail.com>
2022-11-08 22:55:40 +0530

    Basic Math Ques3 palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+string is_palindrome(int x)
+		{
+		    // Code here.
+		    if (x < 0)
+                return "No";
+            vector<int> palin;
+            while( x > 9) {
+                palin.push_back(x%10);
+                x /= 10;
+            }
+            palin.push_back(x);
+            for(int i = 0; i < (palin.size())/2; i++){
+                if (palin[i] != palin[palin.size() - i -1])
+                    return "No";
+            }
+            return "Yes";
+		}
